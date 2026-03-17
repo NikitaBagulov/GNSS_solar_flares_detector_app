4e6afbceefad8b45538f827a2f2d2abf46076b9d
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2026-03-17 22:17:06 +0800

    Refactor main flow into pipeline orchestration layer

---
+++ pipeline/__init__.py
@@ -0,0 +1,15 @@
+from .runner import (
+    PipelineConfig,
+    run_discovery_and_download,
+    run_index_calculation,
+    run_plotting,
+    run_preprocessing,
+)
+
+__all__ = [
+    "PipelineConfig",
+    "run_discovery_and_download",
+    "run_preprocessing",
+    "run_index_calculation",
+    "run_plotting",
+]
